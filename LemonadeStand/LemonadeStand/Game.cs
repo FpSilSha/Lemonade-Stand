be4ff9d50801a69a7ca7dcedde8e4f2b0ec2a4b2
Fp38SilSha <faperez38s@gmail.com>
2019-02-06 10:12:16 -0600

    Game class now has a function to allow the player to play for 'x' amount of days where 'x' is user input.

--- LemonadeStand/LemonadeStand/Game.cs
+++ LemonadeStand/LemonadeStand/Game.cs
@@ -19,12 +19,30 @@ namespace LemonadeStand
             UI.DisplayInfo("How many days would you like to play for? We recommend at least a week(7)!");
             int gameLength = UI.IntegerNumberCheck();
 
+            do
+            {
                 day.StartDay();
 
-            Console.ReadKey();
+
+                Console.ReadKey();
+            }
+            while (gameLength > 0);
+                day.StartDay();
+
+            
           
         }
        
+        public void CustomerCreator()
+        {
+
+        }
         
+        public void WorkingDay()
+        {
+            Customer customer = new Customer();
+            CustomerCreator();
+
+        }
     }
 }
